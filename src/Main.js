0f8ec31846522785c5d8045f1b7779b70ccd47e5
Berlin <berlin_lin@wistron.com>
2021-01-20 14:26:13 +0800

    update

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {
-    StyleSheet, View,
+    StyleSheet, Text,
 } from 'react-native'
 import { Videos } from './video/Videos'
 import { Classify } from './tf/Classify'
@@ -9,11 +9,17 @@ import { TabBar, Icon } from '@ant-design/react-native'
 const styles = StyleSheet.create({
     tabs: {
         color: '#222666'
+    },
+    iconStyles: {
+
+    },
+    title: {
+        fontSize: 13
     }
 });
 
 export const Main = () => {
-    const [selectedTab, setSelectedTab] = useState("Video")
+    const [selectTab, setSelectTab] = useState("Classify")
 
     return (
         <>
@@ -21,23 +27,28 @@ export const Main = () => {
                 unselectedTintColor="#949494"
                 tintColor="#33A3F4"
                 barTintColor="white"
-                hidden={false}
             >
                 <TabBar.Item
-                    title="Video"
+                    title={<Text style={styles.title}>Video</Text>}
                     key="Video"
-                    selected={selectedTab === "Video"}
-                    onPress={() => { setSelectedTab("Video") }}
-                    icon={<Icon name="user" />}
+                    selected={selectTab === "Video"}
+                    onPress={() => {
+                        setSelectTab("Video")
+                    }}
+                    icon={<Icon style={styles.iconStyles} name="desktop" />}
+                    iconStyle={styles.iconStyles}
                 >
                     <Videos />
                 </TabBar.Item>
                 <TabBar.Item
-                    title="Classify"
+                    title={<Text style={styles.title}>Classify</Text>}
                     key="Classify"
-                    selected={selectedTab === "Classify"}
-                    onPress={() => { setSelectedTab("Classify") }}
-                    icon={<Icon name="home" />}
+                    selected={selectTab === "Classify"}
+                    onPress={() => {
+                        setSelectTab("Classify")
+                    }}
+                    icon={<Icon style={styles.iconStyles} name="cluster" />}
+                    iconStyle={styles.iconStyles}
                 >
                     <Classify />
                 </TabBar.Item>
--- src/tf/Classify.js
+++ src/tf/Classify.js
@@ -1,11 +1,56 @@
-import React, { } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     StyleSheet,
-    Text
+    Text,
+    ScrollView,
+    View
 } from 'react-native'
+import {
+    WingBlank,
 
+} from "@ant-design/react-native"
+import * as tf from '@tensorflow/tfjs'
+import { bundleResourceIO } from "@tensorflow/tfjs-react-native"
+
+const styles = StyleSheet.create({
+    hint: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    }
+})
 export const Classify = () => {
+    const [tfReady, setTfReady] = useState(false)
+
+    const tfIsReady = () => {
+        tf.ready()
+            .then(() => {
+                setTfReady(true)
+            })
+    }
+
+    useEffect(() => {
+        tfIsReady()
+    })
+
     return (
-            <Text>Classify</Text>
+        <ScrollView
+            style={{ flex: 1 }}
+            automaticallyAdjustContentInsets={false}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+        >
+            <WingBlank size="lg">
+                <Text style={styles.hint}>TF Ready : {
+                    tfReady ?
+                        <Text style={styles.hint}>OK</Text> :
+                        <Text style={styles.hint}>NG</Text>
+                }</Text>
+            </WingBlank>
+
+            <WingBlank size='lg'>
+
+            </WingBlank>
+        </ScrollView>
     )
 }
